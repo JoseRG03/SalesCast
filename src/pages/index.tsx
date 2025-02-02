f420bd863f5960159223497caffacab1cefc604a
JoseRG03 <114616625+JoseRG03@users.noreply.github.com>
2025-02-02 10:43:43 -0400

    Login Mock Complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
 import { button as buttonStyles } from "@heroui/theme";
 
 import { siteConfig } from "@/config/site";
@@ -13,28 +11,26 @@ export default function IndexPage() {
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
-          <span className={title()}>Make&nbsp;</span>
-          <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
+          <span className={title()}>Planifica&nbsp;</span>
+          <span className={title({ color: "violet" })}>hoy&nbsp;</span>
           <br />
-          <span className={title()}>
-            websites regardless of your design experience.
-          </span>
+          <span className={title()}>Vende mejor&nbsp;</span>
+          <span className={title({ color: "violet" })}>mañana&nbsp;</span>
           <div className={subtitle({ class: "mt-4" })}>
-            Beautiful, fast and modern React UI library.
+            Anticipa las necesidades de tus clientes.
           </div>
         </div>
 
         <div className="flex gap-3">
           <Link
-            isExternal
             className={buttonStyles({
               color: "primary",
               radius: "full",
               variant: "shadow",
             })}
-            href={siteConfig.links.docs}
+            href={siteConfig.links.login}
           >
-            Documentation
+            Iniciar Sesión
           </Link>
           <Link
             isExternal
@@ -42,17 +38,8 @@ export default function IndexPage() {
             href={siteConfig.links.github}
           >
             <GithubIcon size={20} />
-            GitHub
+            Código Fuente
           </Link>
-        </div>
-
-        <div className="mt-8">
-          <Snippet hideCopyButton hideSymbol variant="bordered">
-            <span>
-              Get started by editing{" "}
-              <Code color="primary">pages/index.tsx</Code>
-            </span>
-          </Snippet>
         </div>
       </section>
     </DefaultLayout>
---
+++ src/pages/login.tsx
@@ -0,0 +1,37 @@
+import { Card, CardBody } from "@heroui/card";
+import { Input } from "@heroui/input";
+import { Button } from "@heroui/button";
+import { useNavigate } from "react-router-dom";
+
+import DefaultLayout from "@/layouts/default";
+import { title } from "@/components/primitives";
+
+export const LoginPage = () => {
+  const nav = useNavigate();
+
+  const handleLogin = () => {
+    nav("app/calendar");
+  };
+
+  return (
+    <DefaultLayout>
+      <section className="flex flex-col h-full items-center justify-center gap-4 py-8 md:py-10">
+        <span className={title()}>Iniciar Sesión</span>
+        <Card className="mt-10 w-[50%]">
+          <CardBody className=" px-8 py-16 flex flex-col gap-10">
+            <Input label="Correo Electrónico" radius="full" />
+            <Input label="Contraseña" radius="full" />
+            <Button
+              color="primary"
+              radius="full"
+              variant="shadow"
+              onPress={handleLogin}
+            >
+              Iniciar Sesión
+            </Button>
+          </CardBody>
+        </Card>
+      </section>
+    </DefaultLayout>
+  );
+};
